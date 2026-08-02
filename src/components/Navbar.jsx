93ff929f021f85f4cfc1c9c2d835f9145cd37097
Roshan <pahariroshan55@gmail.com>
2026-08-02 21:02:15 +0545

    static nav

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,35 @@
-
+import logo from "../assets/logo.svg";
 const Navbar = () => {
   return (
     <>
-      <div className="">
-        <h1>This is nav</h1>
-      </div>
-    </>
-  )
-}
+      <header className="px-8">
+        <nav className="mx-auto max-w-7xl flex justify-between items-center py-7 ">
+          <img src={logo} alt="logo" />
 
-export default Navbar
+          <div className=" flex gap-5 font-neue text-lg font-medium  ">
+            <a
+              className="border px-4 py-2 rounded-3xl border-gray-400"
+              href="#"
+            >
+              Work
+            </a>
+            <a
+              className="border px-4 py-2 rounded-3xl border-gray-400"
+              href="#"
+            >
+              Studio
+            </a>
+            <a
+              className="border px-4 py-2 rounded-3xl border-gray-400"
+              href="#"
+            >
+              Contact
+            </a>
+          </div>
+        </nav>
+      </header>
+    </>
+  );
+};
+
+export default Navbar;
