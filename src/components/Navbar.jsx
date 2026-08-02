5d4ccdbf0d5e9f894c2a68827bcc215e8c2bc4ef
Roshan <pahariroshan55@gmail.com>
2026-08-02 21:12:50 +0545

    Refactor Navbar component to use dynamic links mapping

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,30 +1,21 @@
 import logo from "../assets/logo.svg";
 const Navbar = () => {
+  const links = ["Work", "Studio", "Contact"];
   return (
     <>
       <header className="px-8">
-        <nav className="mx-auto max-w-7xl flex justify-between items-center py-7 ">
+        <nav className="mx-auto max-w-8xl flex justify-between items-center py-7 ">
           <img src={logo} alt="logo" />
-
           <div className=" flex gap-5 font-neue text-lg font-medium  ">
-            <a
-              className="border px-4 py-2 rounded-3xl border-gray-400"
-              href="#"
-            >
-              Work
-            </a>
-            <a
-              className="border px-4 py-2 rounded-3xl border-gray-400"
-              href="#"
-            >
-              Studio
-            </a>
-            <a
-              className="border px-4 py-2 rounded-3xl border-gray-400"
-              href="#"
-            >
-              Contact
-            </a>
+            {links.map((link) => {
+             return <a
+                key={link}
+                href=""
+                className="border px-4 py-2 rounded-3xl border-gray-400"
+              >
+                {link}
+              </a>;
+            })}
           </div>
         </nav>
       </header>
